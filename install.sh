296700ce9d23862766be8c40292192ae051f8e17
xisnamh <xisnamh@xisnamh.com>
2026-02-17 00:33:29 +0100

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# ASEGURAR QUE LAS RUTAS RELATIVAS (./MODULES) FUNCIONEN DESDE CUALQUIER CARPETA
+# ASEGURAR QUE LAS RUTAS RELATIVAS FUNCIONEN (./MODULES)
 cd "$(dirname "$0")"
+
+# DESCARGAR FUENTE  ---
+# Definimos la ruta de la fuente parcheada
+FONT_PATH="/usr/share/kbd/consolefonts/ter-u24n-nerd.psf.gz"
+
+if [ ! -f "$FONT_PATH" ]; then
+    printf "${yellow}[*] Preparando entorno visual...${end}\n"
+    # Descargamos solo la fuente de consola (PSF) que soporta iconos
+    # Esta es una versión de Terminus parcheada para TTY
+    curl -sL "https://github.com/terroo/font-terminus-nerd/raw/master/ter-u24n-nerd.psf.gz" -o "$FONT_PATH"
+    
+    # Aplicamos la fuente
+    setfont ter-u24n-nerd
+    sleep 2
+fi
 
 # CARGAR CONFIGURACION Y COLORES
 source ./config.sh
@@ -11,16 +26,16 @@ menu_header
 source ./modules/fuente.sh
 
 menu_header
-source ./modules/discos_particiones.sh
-
-#source ./modules/preconfiguracion.sh
-
-#source ./modules/red.sh
-
-#source ./modules/reflector.sh
-
-#source ./modules/cachyos.sh
-
-#source ./modules/llaves.sh
-
 #source ./modules/discos_particiones.sh
+
+source ./modules/preconfiguracion.sh
+
+source ./modules/red.sh
+
+source ./modules/reflector.sh
+
+source ./modules/cachyos.sh
+
+source ./modules/llaves.sh
+
+source ./modules/discos_particiones.sh
