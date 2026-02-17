fb71a13208c6c5b04770a28a81cf74f0ffe53180
xisnamh <xisnamh@xisnamh.com>
2026-02-17 04:34:44 +0100

    update

--- modules/fuente.sh
+++ modules/fuente.sh
@@ -36,16 +36,14 @@ while true; do
 	esac
 
 	# Aplicamos la fuente para que el usuario vea el cambio
-	setfont "$FONT" 2>/dev/null
-
-	# Fuerza a la TTY a refrescar el modo UTF-8
-	reset -e 2>/dev/null
-	unicode_start 2>/dev/null
+setfont "$FONT" -u unicode 2>/dev/null
+unicode_start "$FONT" 2>/dev/null
+printf "\033%%G"
 
 	# Usamos la nueva funcion de confirmacion
 	if print_confirm "Te gusta este tamaño?"; then
 		printf "${green}[${ico_ok}] Fuente configurada.${end}\n"
-		sleep $T_ERR
+		sleep $T_INFO
 		break
 	else
 		# Si no le gusta, restauramos la fuente por defecto y vuelve al inicio del bucle
