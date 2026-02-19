f7283f9c3df97ea300a97ec4a07cb2411d5588f2
xisnamh <xisnamh@xisnamh.com>
2026-02-19 06:14:14 +0100

    update

--- modules/red.sh
+++ modules/red.sh
@@ -12,7 +12,7 @@ while true; do
 	fi
 
 	printf "\n${purple}Estado actual de las interfaces:${end}\n"
-	ip -brief address show scope global
+ip -4 -brief address show scope global | awk '{print $1, $2, $3}' | column -t
 	printf "\n"
 
 	WIFI_DEVICES=$(iwctl device list | grep -E "wlan|p2p")
