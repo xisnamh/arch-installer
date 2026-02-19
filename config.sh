a864a74114009070685de22c135038ae41d023ac
xisnamh <xisnamh@xisnamh.com>
2026-02-19 04:59:59 +0100

    update

--- config.sh
+++ config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # COLORES ANSI
-export blue="\033[1;36m"	# Azul suave		(titulo)
-export white="\e[33;1;37m"	# Blanco puro		(subtitulo)
+export_blue='\033[0;34m'     # Azul oscuro (serio, tipo IBM/Arch)
+arch_cyan='\033[0;36m'     # Cian mate (parecido al logo pero sin brillo)
+#export blue="\e[38;5;81m"	# Azul suave		(titulo)
+export white="\e[38;5;255m"	# Blanco puro		(subtitulo)
 export red="\e[38;5;196m"	# Rojo			(alerta critica)
 export orange="\e[38;5;216m"	# Naranja		(aviso importante)
 export lime="\e[38;5;118m"	# Verde lima		(exito)
