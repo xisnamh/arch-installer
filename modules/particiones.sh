3821c118bc18abaa1dc5118274f7d8a71cea57ec
xisnamh <xisnamh@xisnamh.com>
2026-02-21 03:52:26 +0100

    update

--- modules/particiones.sh
+++ modules/particiones.sh
@@ -3,20 +3,20 @@
 # 8. FORMATEAR PARTICIONES
 
 # Funcion para gestionar la entrada y ejecucion de cada comando de formateo
-ejecutar_formateo() {
+exec_format() {
 	local TAREA="$1"
 	local COMANDO_BASE="$2"
 	local PARTICION
 
 	while true; do
 		menu_header
-		print_title "PARTICIONES"
+		print_title "PARTICIONES: FORMATEAR"
 		printf "${gray}Configurando las particiones:${end}\n"
 		lsblk -p -o NAME,SIZE,TYPE,FSTYPE
 		printf "\n"
 		
-		print_info "Tarea actual: $TAREA"
-		printf "${ico_input} Escribe la ruta para (/dev/...): ${end}"
+		printf "${ico_star}${greend} $TAREA"
+		printf "${ico_input} Escribe la ruta EFI: ${end}"
 		read -r PARTICION
 
 		if [ -z "$PARTICION" ] || [ ! -b "$PARTICION" ]; then
@@ -25,13 +25,13 @@ ejecutar_formateo() {
 			continue
 		fi
 
-		if print_confirm "¿Estas seguro de formatear $PARTICION?"; then
+		if print_confirm "¿Estas seguro de formatear la particion?"; then #(ESTE MENSAJE SE VA A REPETIR 4 VECES, ALGO PARA NO REPETIRLO 4 VECES??)
 			printf "\n${ico_star} ${greend}${TAREA}...${end}\n"
 			
 			# Ejecucion del comando
 			if $COMANDO_BASE "$PARTICION"; then
 				printf "${ico_ok} ${greenl}Completado.${end}\n"
-				sleep $T_INFO
+				print_continue
 				break
 			else
 				printf "${ico_error} ${redl}Error al ejecutar el comando.${end}\n"
@@ -45,22 +45,19 @@ ejecutar_formateo() {
 	done
 }
 
-# --- INICIO DEL PROCESO DE FORMATEO ---
+# PROCESO DE FORMATEO
+# Particion efi
+exec_format "Formateando la particion EFI." "mkfs.fat -F32"
 
-# 1. PARTICION EFI
-ejecutar_formateo "Formateando particion EFI (FAT32)" "mkfs.fat -F32"
+# Particion root
+exec_format "Formateando la particion ROOT." "mkfs.ext4 -F"
 
-# 2. PARTICION ROOT
-ejecutar_formateo "Formateando particion ROOT (EXT4)" "mkfs.ext4 -F"
+# Particion juegos
+exec_format "Formateando la particion JUEGOS." "mkfs.f2fs -f -O extra_attr,inode_checksum,sb_checksum,compression"
 
-# 3. PARTICION JUEGOS
-ejecutar_formateo "Formateando particion JUEGOS (F2FS)" "mkfs.f2fs -f -O extra_attr,inode_checksum,sb_checksum,compression"
+# Particion hdd
+exec_format "Formateando la particion HDD." "mkfs.ext4 -F"
 
-# 4. PARTICION HDD
-ejecutar_formateo "Formateando particion HDD (EXT4)" "mkfs.ext4 -F"
-
-# --- FINALIZACION ---
 menu_header
-print_title "PARTICIONES"
-printf "${ico_ok} ${greenl}Configuracion de particiones finalizada correctamente.${end}\n"
+print_title "PARTICIONES: FORMATEAR."
 print_continue
